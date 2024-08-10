b2081cf471549b36a5deca5092e1b86c9f301d49
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2024-08-10 08:39:45 +0200

    problem in signin commits

---
+++ BreakingChanges/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MongoDB.Driver;
+using Rebus.Activation;
+using Rebus.Config;
+
+var conn = new MongoUrl("mongodb://localhost:27017");
+var client = new MongoClient(conn);
+var db = client.GetDatabase("test");
+
+var someContainerAdapter = new BuiltinHandlerActivator();
+var config = Rebus.Config.Configure
+    .With(someContainerAdapter)
+    .Subscriptions(s => s.StoreInMongoDb(db, "TEST"));
+Console.WriteLine("Hello, World!");
